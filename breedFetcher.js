fe85c6b21d62916604b922c5246f9caa68f6e0db
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-04 20:45:49 +0000

    Tests added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require("request");
 const userinput = process.argv.slice(2);
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${userinput}`,
-  (error, response, body) => {
-    if (error !== null) {
-      console.log(error);
-    } else {
-      const data = JSON.parse(body);
 
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${userinput}`,
+    (error, response, body) => {
+      if (error !== null) {
+        //console.log(error);
+        callback(error, null);
+        return;
+      }
+
+      const data = JSON.parse(body);
       if (data[0] === undefined) {
-        console.log("Sorry breed not found");
-      } else console.log(data[0].description);
+        callback(console.log("Sorry breed not found"), null);
+      } else callback(null, data[0].description);
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
